a77d5312b35bab432ee90f0b16c84d238140c79b
Aaron Zhang <aaronzh91@gmail.com>
2018-12-10 00:12:01 -0500

    added mysql connection

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
+// Connect string to MySQL
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'rds-mysql-cis550movie.cse71gfxjvmn.us-east-1.rds.amazonaws.com',
+  user     : 'CIS550Movie',
+  password : 'cis550movie',
+  database : 'dbMovie'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
 
 module.exports = router;
