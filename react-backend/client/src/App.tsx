998c0791157d6516d8b3ebba0606c8d547a1e161
Aaron Zhang <aaronzh91@gmail.com>
2018-12-12 00:02:10 -0500

    working version

--- react-backend/client/src/App.tsx
+++ react-backend/client/src/App.tsx
@@ -1,48 +1,8 @@
-import * as React from 'react';
-import './App.css';
-import Footer from './Footer';
-import ListArea from './ListArea';
-import Navi from './Navigation';
-
-class App extends React.Component {
-
-  public state = {
-    data: [],
-    page: false
-  }
-
-  public refreshData(d: any) {
-    this.setState({ data: d })
-  }
-
-  public changePages(b: boolean) {
-    this.setState({ page: b });
-  }
-
-  public render() {
-    // TODO To display different page, modify the content of Navi and ListArea based on the value of page
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">{this.state.page ? "Tradition" : "Snuggle"}</h1>
-        </header>
-        <div className="mood"><i>In the mood for...</i></div>
-        <Navi that={this} page={this.state.page} />
-        <ListArea data={this.state.data} page={this.state.page} />
-        <Footer that={this} />
-      </div>
-    );
-  }
-}
-
-export default App;
-
 // import * as React from 'react';
 // import './App.css';
 // import Footer from './Footer';
 // import ListArea from './ListArea';
 // import Navi from './Navigation';
-
 
 // class App extends React.Component {
 
@@ -51,23 +11,11 @@ export default App;
     // page: false
   // }
 
-  // public refreshData(url: string) {
-        // // TODO Parsing and processing data
-        // const loader = new XMLHttpRequest();
-        // // loader.open("GET", "/spy_movies", true);
-		// loader.open("GET", url, false);
-        // loader.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-        // loader.send();
-		// if (loader.status === 200) {
-			// console.log(loader.responseText);
-			// const d = loader.response;
-			// this.setState({ data: d });
-		// } else {
-			// console.log('error');
-		// }				
+  // public refreshData(d: any) {
+    // this.setState({ data: d })
   // }
 
-  // public changePages =(b: boolean) =>{
+  // public changePages(b: boolean) {
     // this.setState({ page: b });
   // }
 
@@ -87,4 +35,66 @@ export default App;
   // }
 // }
 
-// export default App
+// export default App;
+
+import * as React from 'react';
+import './App.css';
+import Footer from './Footer';
+import ListArea from './ListArea';
+import Navi from './Navigation';
+
+
+class App extends React.Component {
+
+  public state = {
+    data: [],
+    page: false
+  }
+
+  // public refreshData(url: string) {
+        // // TODO Parsing and processing data
+        // const loader = new XMLHttpRequest();
+        // // loader.open("GET", "/spy_movies", true);
+		// loader.open("GET", url, false);
+        // loader.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+        // loader.send();
+		// if (loader.status === 200) {
+			// console.log(loader.responseText);
+			// const d = loader.response;
+			// this.setState({ data: d });
+		// } else {
+			// console.log('error');
+		// }				
+  // }
+  
+  public refreshData(url: string) {
+	  fetch(url)
+		.then(res => res.json())
+		.then(json => {
+			this.setState({
+				data: json
+			})
+		});
+  }
+
+  public changePages =(b: boolean) =>{
+    this.setState({ page: b });
+  }
+
+  public render() {
+    // TODO To display different page, modify the content of Navi and ListArea based on the value of page
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">{this.state.page ? "Tradition" : "Snuggle"}</h1>
+        </header>
+        <div className="mood"><i>In the mood for...</i></div>
+        <Navi that={this} page={this.state.page} />
+        <ListArea data={this.state.data} page={this.state.page} />
+        <Footer that={this} />
+      </div>
+    );
+  }
+}
+
+export default App
--- react-backend/client/src/Navigation.tsx
+++ react-backend/client/src/Navigation.tsx
@@ -1,29 +1,63 @@
+// import * as React from 'react';
+// import App from './App';
+// import './Navigation.css';
+
+// class Navi extends React.Component {
+
+    // public props: { that: App | null, page: boolean | null } = {
+        // page: null,
+        // that: null
+    // }
+
+    // public httpLoader(url: string) {
+        // // TODO Parsing and processing data
+        // const loader = new XMLHttpRequest();
+        // loader.open("GET", url, true);
+        // loader.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        // loader.send();
+		// console.log(loader.responseText);
+        // const that = this.props.that;
+        // if (that) {
+             // that.refreshData(loader.response);
+        // }
+    // }
+
+
+    // public render() {
+        // // TODO replace url here
+        // return (
+            // <div className="Navi">
+                // <div className="NaviItem" onClick={this.httpLoader.bind(this, "/adventure")}><a href="#">I'm adventurous</a></div>
+                // <div className="NaviItem" onClick={this.httpLoader.bind(this, "/cuddling")}><a href="#">Cuddling</a></div>
+                // <div className="NaviItem" onClick={this.httpLoader.bind(this, "/brad_pitt")}><a href="#">Brad Pitt</a></div>
+                // <div className="NaviItem" onClick={this.httpLoader.bind(this, "/spy_movies")}><a href="#">Spy Movies</a></div>
+            // </div>
+        // );
+    // }
+// }
+
+// export default Navi;
+
+
 import * as React from 'react';
 import App from './App';
 import './Navigation.css';
 
-class Navi extends React.Component {
+export interface INaviProp {
+	page: boolean; 
+	that: App; 
+}
 
-    public props: { that: App | null, page: boolean | null } = {
-        page: null,
-        that: null
-    }
+class Navi extends React.Component <INaviProp>{
 
-    public httpLoader(url: string) {
-        // TODO Parsing and processing data
-        const loader = new XMLHttpRequest();
-        loader.open("GET", url, true);
-        loader.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        loader.send();
-        const that = this.props.that;
-        if (that) {
-             that.refreshData(loader.response);
-            // that.refreshData([{
-                // link: "https://www.qq.com",
-                // src: "https://img5.duitang.com/uploads/item/201601/08/20160108212022_YPkGR.jpeg",
-                // title: "1",
-            // }]);
-        }
+	 constructor(props:any) {
+			super(props); 
+	}
+		
+    public httpLoader = (url: string)=> {
+		if (this.props.that) {
+			this.props.that.refreshData(url);
+		}
     }
 
     public render() {
@@ -40,40 +74,3 @@ class Navi extends React.Component {
 }
 
 export default Navi;
-
-
-// import * as React from 'react';
-// import App from './App';
-// import './Navigation.css';
-
-// export interface INaviProp {
-	// page: boolean; 
-	// that: App; 
-// }
-
-// class Navi extends React.Component <INaviProp>{
-
-	 // constructor(props:any) {
-			// super(props); 
-	// }
-		
-    // public httpLoader = (url: string)=> {
-		// if (this.props.that) {
-			// this.props.that.refreshData(url);
-		// }
-    // }
-
-    // public render() {
-        // // TODO replace url here
-        // return (
-            // <div className="Navi">
-                // <div className="NaviItem" onClick={this.httpLoader.bind(this, "/adventure")}><a href="#">I'm adventurous</a></div>
-                // <div className="NaviItem" onClick={this.httpLoader.bind(this, "/cuddling")}><a href="#">Cuddling</a></div>
-                // <div className="NaviItem" onClick={this.httpLoader.bind(this, "/brad_pitt")}><a href="#">Brad Pitt</a></div>
-                // <div className="NaviItem" onClick={this.httpLoader.bind(this, "/spy_movies")}><a href="#">Spy Movies</a></div>
-            // </div>
-        // );
-    // }
-// }
-
-// export default Navi;
